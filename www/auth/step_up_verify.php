2ba9790a2355ce428dde0d6611074940699ccad2
Timikana <timi.du.10@gmail.com>
2026-05-19 21:23:12 +0200

    fix(vague3): step_up_verify - require vendor/autoload pour OTPHP\TOTP
    Oubli mineur du commit precedent : OTPHP\TOTP est une classe Composer qui necessite vendor/autoload.php. Symptomatique de la 1ere call : Fatal error 'Class OTPHP\TOTP not found'. Aligne sur le pattern de verify_2fa.php.

--- www/auth/step_up_verify.php
+++ www/auth/step_up_verify.php
@@ -13,6 +13,7 @@
  */
 
 require_once __DIR__ . '/../db.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/verify.php';
 require_once __DIR__ . '/../includes/totp_crypto.php';
 require_once __DIR__ . '/step_up.php';
